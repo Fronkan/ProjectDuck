1d081746fdb26fd8608aa16fec85f67716c13729
Fredrik Bengtsson <frebe780@student.liu.se>
2016-11-11 23:15:23 +0100

    added script for controlling duck

---
+++ Assets/scriptsCharacters/duckController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class duckController : MonoBehaviour {
+
+    public float speed = 0.2f;
+    private bool faceingRight = true;
+    private CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+        characterController = gameObject.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       float move = Input.GetAxis("Horizontal");
+        gameObject.GetComponent<CharacterController>().Move(new Vector3(move*speed, 0, 0));
+       
+        if (move < 0 && faceingRight) {
+            Flip();
+        }
+
+        else if (move > 0 && !faceingRight) {
+            Flip();
+        }
+        
+    }
+
+    void Flip() {
+        faceingRight = !faceingRight;
+        Vector3 scale = transform.localScale;
+        scale.x = -1*scale.x;
+        transform.localScale = scale;
+    }
+}
