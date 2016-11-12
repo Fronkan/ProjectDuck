88743f458724358122e768dc3f0d027c8cbc9b88
Fredrik Bengtsson <frebe780@student.liu.se>
2016-11-12 12:26:12 +0100

    added healthSystem script

---
+++ Assets/scriptsCharacters/healthSystem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class healthSystem : MonoBehaviour {
+
+    public int health = 10;
+    public int dmgModifier = 0;
+
+    public void Damage() {
+        Debug.Log(gameObject.name + ": DAMAGE TAKEN");
+
+    }
+}
