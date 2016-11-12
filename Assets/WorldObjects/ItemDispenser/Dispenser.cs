678e1c90219bf7fc35c10317c0a544803f1003c0
Hugo Hörnquist <hugo.hornquist@gmail.com>
2016-11-12 15:45:02 +0100

    added arrow and dispenser prefabs

---
+++ Assets/WorldObjects/ItemDispenser/Dispenser.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dispenser : SignalReciever {
+
+	public Vector2 DispenseSpeedDirection;
+	public GameObject DispensedObject;
+	public float BulletLifeTime;
+
+	void Dispense() {
+		GameObject clone = Object.Instantiate(DispensedObject);
+		clone.transform.position = gameObject.transform.position;
+		clone.GetComponent<Rigidbody2D>().velocity = DispenseSpeedDirection;
+
+		Destroy(clone, BulletLifeTime);
+	}
+
+	void OnMouseDown() {
+		Dispense();
+	}
+
+	void RecieveSignal() {
+		Dispense();
+	}
+}
