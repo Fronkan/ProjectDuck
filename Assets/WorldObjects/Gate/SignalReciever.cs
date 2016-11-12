c092776693c22077c3d29b72ab81bec2868a1614
Hugo Hörnquist <hugo.hornquist@gmail.com>
2016-11-12 13:54:02 +0100

    added gate and pressure plate

---
+++ Assets/WorldObjects/Gate/SignalReciever.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class SignalReciever : MonoBehaviour {
+
+	void Start() { }
+	void Update() { }
+
+	void RecieveSignal() {
+		Debug.Log("Recieved signal");
+	}
+}
