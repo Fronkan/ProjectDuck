ef3fcd3ab50af28f99234cecb30b9043d7936ade
Fredrik Bengtsson <frebe780@student.liu.se>
2016-11-12 22:22:22 +0100

    added victory system with 2 victory points

---
+++ Assets/Victory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Victory : MonoBehaviour {
+    private VictoryPoint point1;
+    private VictoryPoint point2;
+
+	// Use this for initialization
+	void Start () {
+        point1 = transform.GetChild(0).GetComponent<VictoryPoint>();
+        point2 = transform.GetChild(1).GetComponent<VictoryPoint>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (point1.isVictory && point2.isVictory) {
+            //TODO: Add victory screen and level changer
+            Debug.Log("VICTORY");
+        }
+	}
+}
---
+++ Assets/VictoryPoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictoryPoint : MonoBehaviour {
+    public bool isVictory = false;
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        isVictory = collider.CompareTag("Player");
+    }
+    void OnTriggerExit2D()
+    {
+        isVictory = false;
+    }
+}
