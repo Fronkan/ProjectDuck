893346925c5b989481dfc133c5caed10111a6b68
Fredrik Bengtsson <frebe780@student.liu.se>
2016-11-12 19:11:49 +0100

    scrits for triggerble particleSystem

---
+++ Assets/particleSystems/Scripts/fire.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class fire : MonoBehaviour {
+    public GameObject button;
+    private ParticleSystem dispensor;
+	// Use this for initialization
+	void Start () {
+        dispensor = GetComponent<ParticleSystem>();
+        dispensor.Pause();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (button.GetComponent<trigger>().fire)
+        {
+            if (!dispensor.isPlaying){
+                dispensor.Play();
+            }
+            Debug.Log("START");
+        }
+        else
+        {
+            if (dispensor.isPlaying){ dispensor.Stop(); }
+            Debug.Log("STOP");
+        }
+    }
+}
---
+++ Assets/particleSystems/Scripts/trigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trigger : MonoBehaviour {
+    public bool fire = false;
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            fire = true;
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            fire = false;
+        }
+    }
+
+}
