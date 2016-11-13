a98cc0e49abdadd0f28300ca366ab66ea66d12d6
Hugo Hörnquist <hugo.hornquist@gmail.com>
2016-11-13 15:32:35 +0100

    dispenser now takes offset

--- Assets/WorldObjects/ItemDispenser/Dispenser.cs
+++ Assets/WorldObjects/ItemDispenser/Dispenser.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class Dispenser : SignalReciever {
 
 	public Vector2 DispenseSpeedDirection;
+	public Vector2 dispenseOffset;
 	public GameObject DispensedObject;
 	public float BulletLifeTime;
 
 	void Dispense() {
 		GameObject clone = Object.Instantiate(DispensedObject);
-		clone.transform.position = gameObject.transform.position;
+		clone.transform.position = gameObject.transform.position + dispenseOffset;
 		clone.GetComponent<Rigidbody2D>().velocity = DispenseSpeedDirection;
 
 		Destroy(clone, BulletLifeTime);
