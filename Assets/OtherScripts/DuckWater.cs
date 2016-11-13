a41b107409c05ae3f0419fad75e6fc5c3a365241
Hugo Hörnquist <hugo.hornquist@gmail.com>
2016-11-13 13:34:16 +0100

    added duck water

---
+++ Assets/OtherScripts/DuckWater.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckWater : MonoBehaviour {
+	
+	void OnTriggerEnter2D(Collider2D target) {
+		if(target.name == "duck") {
+			Vector2 pos = target.gameObject.transform.position;
+			target.gameObject.transform.position
+				= new Vector2(pos.x, transform.position.y + 5);
+		}
+	}
+}
