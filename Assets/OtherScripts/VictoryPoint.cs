e13683fa9fb750c2f735ee164d69beab4e3a6815
Isak Jonason <isakjonason@gmail.com>
2016-11-13 17:22:09 +0100

    klart

--- Assets/OtherScripts/VictoryPoint.cs
+++ Assets/OtherScripts/VictoryPoint.cs
@@ -4,10 +4,10 @@ using System.Collections;
 public class VictoryPoint : MonoBehaviour {
     public bool isVictory = false;
 
-    void OnTriggerEnter2D(Collider2D collider) {
+    void OnTriggerStay2D(Collider2D collider) {
         isVictory = collider.CompareTag("Player");
     }
-    void OnTriggerExit2D()
+    void OnTriggerExit2D(Collider2D collider)
     {
         isVictory = false;
     }
